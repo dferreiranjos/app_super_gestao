f484751fdf9050e132b2141efbc5095b2825b676
dferreiranjos <dferreiranjos@yahoo.com.br>
2023-08-09 20:53:58 -0300

    fix model for use eloquent

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+
+    // O eloquent ORM apenas coloca um s no final do nome da classe para encontrar a tabela correspondente no banco de dados. Assim, preciso definir aqui a tabela correta. 
+
+    protected $table = 'fornecedores';
 }
