571caee91ce926ffa2fd4b25af357e09672af631
dferreiranjos <dferreiranjos@yahoo.com.br>
2023-08-22 21:53:45 -0300

    Validation form contato

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -37,12 +37,46 @@ class ContatoController extends Controller
 
         // O metodo fill preenche o objeto através de um array associativo. Posso usar o create no lugar do fill e nesse caso não preciso usa o método save().
         // OBS: É preciso definir o protected fillable na model
-        $contato = new SiteContato();
-        $contato->fill($request->all());
+        // $contato = new SiteContato();
+        // $contato->fill($request->all());
         // print_r($contato->getAttributes());
-        $contato->save();
+        // $contato->save();
+
+        $motivo_contatos = [
+            '1'=>'Dúvida',
+            '2'=>'Elogio',
+            '3'=>'Reclamação'
+        ];
 
         $title = 'Contato';
-        return view('site.contato', compact('title'));
+        return view('site.contato', ['title'=>'Contatos', 'motivo_contatos'=>$motivo_contatos]);
+    }
+
+    public function salvar(Request $request)
+    {
+        $regras =             [
+            'nome' => 'required|min:3|max:40|unique:site_contatos',
+            'telefone' => 'required',
+            'email' => 'email',
+            'motivo_contatos_id' => 'required',
+            'mensagem' => 'required|max:2000'
+        ];
+
+        $feedback = [
+            'nome.min' => 'O campo nome precisa ter no mínimo 3 caracteres',
+            'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+            'nome.unique' => 'O nome informado já está em uso',
+
+            'email.email' => 'O email informado não é válido',
+
+            'mensagem.max' => 'A mensagem deve ter no máximo 2000 caracteres',
+
+            'required' => 'O campo :attribute deve ser preenchido'
+        ];
+
+        $request->validate($regras, $feedback);
+
+        // SiteContato::create($request->all());
+        // return redirect()->route('site.index');
     }
 }
